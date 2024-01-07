88194f84dd3d947a7101f9e5432f30d2a2e88103
Shauryacious <shauryaagrawal2718@gmail.com>
2024-01-07 12:12:06 +0530

    css and styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
+import React from "react";
+import { ReactDOM } from "react-dom/client";
+import './App.css';
+
 function App() {
   return (
-    <div className="App">
-      
+    <div className="Container">
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
+}
+
+function Header(){
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  )
+}
+function Menu(){
+  return(
+    <main className="menu">
+      <h2>Our Menu</h2>
+      <Pizza />
+    </main>
+  )
+}
+
+function Pizza(){
+  return(
+    <h1>Pizza component</h1>
+  )
+}
+
+
+function Footer(){
 }
 
 export default App;
